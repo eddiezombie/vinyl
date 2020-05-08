2ea8559f01198aeb3c2d29a1507623a7baa830ed
Edinson Rodriguez <edinsonro@gmail.com>
2020-05-07 22:40:20 -0400

    chore: add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './components/nav/nav.component';
+import { ListenComponent } from './components/listen/listen.component';
+import { CardComponent } from './components/card/card.component';
+import { ListComponent } from './components/list/list.component';
+import { SingleComponent } from './components/single/single.component';
+import { AddComponent } from './components/add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ListenComponent,
+    CardComponent,
+    ListComponent,
+    SingleComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
