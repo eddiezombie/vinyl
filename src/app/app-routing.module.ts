f68b8278656888c345289752e1f195e4a19ab5ec
Edinson Rodriguez <edinsonro@gmail.com>
2020-05-07 23:09:01 -0400

    feat: add rutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
+import { ListenComponent } from './components/listen/listen.component';
+import { CardComponent } from './components/card/card.component';
+import { ListComponent } from './components/list/list.component';
+import { SingleComponent } from './components/single/single.component';
+import { AddComponent } from './components/add/add.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListenComponent },
+  { path: 'listen', component: ListenComponent },
+  { path: 'card', component: CardComponent },
+  { path: 'list', component: ListComponent },
+  { path: 'single', component: SingleComponent },
+  { path: 'add', component: AddComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
